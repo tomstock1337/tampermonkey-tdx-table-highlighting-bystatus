3c5263ec1cfd3269c0916c6d45e57f2613bb5f45
thstockw <thstockw@umich.edu>
2023-05-05 14:19:11 -0400

    Adjusted match strings to include new url variations

--- tampermonkey-tdx-table-highlighting-bystatus.user.js
+++ tampermonkey-tdx-table-highlighting-bystatus.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name         TDNext Table Highlighting
 // @namespace    https://www.thomasstockwell.com
-// @version      0.11
+// @version      0.12
 // @description  try to take over the world!
 // @author       You
 
-// @match        https://*.teamdynamix.com/TDNext/Apps/569/Tickets/Desktop.aspx
-// @match        https://*.teamdynamix.com/TDNext/Apps/569/Tickets/Default.aspx
+// @match        https://*.teamdynamix.com/TDNext/Apps/*Tickets/Desktop.aspx
+// @match        https://*.teamdynamix.com/TDNext/Apps/*Tickets/Default.aspx
 // @match        https://*.teamdynamix.com/TDNext/Home/Desktop/Desktop.aspx
 // @match        https://*.teamdynamix.com/TDNext/Apps/People/PersonTickets.aspx*
-// @match        https://*.teamdynamix.com/TDNext/Apps/569/Reporting/ReportViewer*
-// @match        https://*.teamdynamix.com/TDNext/Apps/569/Tickets/TicketSearch*
+// @match        https://*.teamdynamix.com/TDNext/Apps/*Reporting/ReportViewer*
+// @match        https://*.teamdynamix.com/TDNext/Apps/*Tickets/TicketSearch*
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @downloadURL  https://github.com/tomstock1337/tampermonkey-tdx-table-highlighting-bystatus/raw/main/tampermonkey-tdx-table-highlighting-bystatus.user.js
